5c5a97744c4e04f936f8dc0c4913e5305ae79dc7
jixinyuan <496509528@qq.com>
2019-09-30 14:18:42 +0800

    1

--- SSM_0820/src/com/ssm07/service/StuService.java
+++
@@ -1,9 +0,0 @@
-package com.ssm07.service;
-
-import com.ssm07.entity.Student;
-
-public interface StuService {
-	Student cha(int sid);
-	int updatePic(String touxiang);
-	int update(Student stu);
-}
